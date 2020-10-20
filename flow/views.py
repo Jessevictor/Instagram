b00303a4b7acd384c2fabc7bf31d44524e01daac
jessevictor <kongoijesse97@gmail.com>
2020-10-20 05:53:37 +0300

    Add view funcion for new post

--- flow/views.py
+++ flow/views.py
@@ -1,9 +1,26 @@
-from django.shortcuts import render
-from .models import instagram,Caption
-# Create your views here.
-def index(request): 
-    return render(request, 'index.html')
+from django.http  import HttpResponse,Http404
+from django.shortcuts import render, redirect
+from .forms import NewPostForm
+from .models import Image
 
-def insta(request):
-    my_image = instagram.all_images()
-    return render(request, 'instagram.html', {"my_image":my_image})
+def home(request):
+    all_posts = Image.all_images()
+    print(all_posts)
+    return render(request, 'index.html', {'all_posts':all_posts})
+
+def new_post(request):
+    if request.method == 'POST':
+        form = NewPostForm(request.POST, request.FILES)
+
+        if form.is_valid():
+            image = form.save(commit = False)
+            image.image_poster = request.user
+            image.save()
+        return redirect('index')
+
+    else:
+        form = NewPostForm()
+    return render(request, 'post_image.html', {'form': form})
+
+def profile(request):
+    return render(request, 'profile.html')
